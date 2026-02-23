3924ca3ef2fde50c34d7e4e4a69c5f4aa3bf783b
UTOOK <NT4-Wildbranch@proton.me>
2026-02-22 23:44:04 -0800

    Added missing image for flow tab

--- bin/testermon.bat
+++ bin/testermon.bat
@@ -1,1 +1,3 @@
+set /p answer=Destroy Previous System State? [Y/N]:
+if "%answer%" EQU "Y" del ./System_State_Files/ *.*
 cls && cd .. && builderman.bat && cd ..\..\bin && start python GUI_Op.0.0.py && NT4
